f1b56f39a794fba070b5de3c222dd80bb25028d2
Bruno Sales <me@baliestri.dev>
2022-12-07 14:56:18 -0300

    feat(web-api/controllers): add `ErrorController`

---
+++ source/WebAPI/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFinances.WebAPI.Controllers;
+
+/// <summary>
+///   Controller for handling errors.
+/// </summary>
+public sealed class ErrorController : ControllerBase {
+  /// <summary>
+  ///   Handles errors.
+  /// </summary>
+  /// <returns>The error response.</returns>
+  [Route("api/error")]
+  public IActionResult Error() {
+    var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+    var exception = context?.Error;
+
+    return Problem(title: exception?.Message);
+  }
+}
