17869574427aa2edad8cfb277726829417353033
Bruno Sales <me@baliestri.dev>
2022-12-07 18:08:35 -0300

    feat(application/validation): update extensions for `IServiceCollection`
    add `AddMediatRBehaviors`
    
    invoke `AddValidatorsFromAssembly`

--- source/Application/ServiceCollectionExtensions.cs
+++ source/Application/ServiceCollectionExtensions.cs
@@ -1,12 +1,21 @@
 ﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
 // See the LICENSE file in the repository root for full license text.
 
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using MyFinances.Application.Behaviors;
 
 namespace MyFinances.Application;
 
 public static class ServiceCollectionExtensions {
   public static IServiceCollection AddApplicationLayer(this IServiceCollection serviceCollection)
-    => serviceCollection.AddMediatR(typeof(ServiceCollectionExtensions).Assembly);
+    => serviceCollection
+      .AddMediatR(typeof(ServiceCollectionExtensions).Assembly)
+      .AddMediatRBehaviors()
+      .AddValidatorsFromAssembly(typeof(ServiceCollectionExtensions).Assembly);
+
+  private static IServiceCollection AddMediatRBehaviors(this IServiceCollection serviceCollection)
+    => serviceCollection
+      .AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 }
