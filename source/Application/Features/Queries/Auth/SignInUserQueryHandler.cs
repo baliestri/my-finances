560c0b56c62d5a12eebfe7bcc58130e584b669dc
Bruno Sales <me@baliestri.dev>
2022-12-06 23:34:23 -0300

    feat(application/cqrs): add `SignInUserQuery`

---
+++ source/Application/Features/Queries/Auth/SignInUserQueryHandler.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using ErrorOr;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using MyFinances.Application.Abstractions.Providers;
+using MyFinances.Application.Contracts.Responses;
+using MyFinances.Application.Errors;
+using MyFinances.Core.Abstractions.Persistence.Repositories;
+
+namespace MyFinances.Application.Features.Queries.Auth;
+
+public sealed class SignInUserQueryHandler : IRequestHandler<SignInUserQuery, ErrorOr<SuccessfulAuthResponse>> {
+  private readonly ILogger<SignInUserQueryHandler> _logger;
+  private readonly IJwtTokenProvider _tokenProvider;
+  private readonly IUserRepository _userRepository;
+
+  public SignInUserQueryHandler(
+    ILogger<SignInUserQueryHandler> logger, IJwtTokenProvider tokenProvider, IUserRepository userRepository
+  ) {
+    _logger = logger;
+    _tokenProvider = tokenProvider;
+    _userRepository = userRepository;
+  }
+
+  public async Task<ErrorOr<SuccessfulAuthResponse>> Handle(
+    SignInUserQuery request, CancellationToken cancellationToken
+  ) {
+    var (userName, email, password) = request;
+    _logger.LogInformation("Attempting to sign in user {UserName} ({Email})", userName, email);
+
+    var isUserNameEmpty = string.IsNullOrWhiteSpace(userName);
+    var isEmailEmpty = string.IsNullOrWhiteSpace(email);
+
+    if ((isEmailEmpty && !isUserNameEmpty) ||
+        (isUserNameEmpty && !isEmailEmpty)) {
+      _logger.LogWarning("The username or email was not provided");
+      return AuthErrors.UserNameOrEmailFieldRequired;
+    }
+
+    if (!isUserNameEmpty &&
+        !isEmailEmpty) {
+      _logger.LogWarning("Both username and email were provided, using email");
+    }
+
+    var user = isEmailEmpty
+      ? await _userRepository.FindByUserNameAsync(userName!)
+      : await _userRepository.FindByEmailAsync(email!);
+
+    if (user is null) {
+      _logger.LogWarning("The provided username or email was not found");
+      return AuthErrors.InvalidCredentials;
+    }
+
+    if (!user.Password.Equals(password)) {
+      _logger.LogWarning("The provided password does not match");
+      return AuthErrors.InvalidCredentials;
+    }
+
+    var accessToken = _tokenProvider.GenerateAccessToken(user.Id, user.UserName, user.Email);
+    var accessTokenExpirationDate = _tokenProvider.GetExpirationDate(accessToken);
+    _logger.LogInformation("User signed in successfully");
+
+    return new SuccessfulAuthResponse(accessToken, accessTokenExpirationDate);
+  }
+}
