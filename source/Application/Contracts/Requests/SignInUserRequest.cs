c00865fc39a93661fd7f057bfbc6856d8fc33da9
Bruno Sales <me@baliestri.dev>
2022-12-06 22:32:58 -0300

    feat(application/contracts): add `SignInUserRequest`

---
+++ source/Application/Contracts/Requests/SignInUserRequest.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+namespace MyFinances.Application.Contracts.Requests;
+
+public sealed record SignInUserRequest(
+  string? UserName,
+  string? Email,
+  string Password
+);
