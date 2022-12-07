cf2ac6925a391521bb2c68bd6494f2f3a545d9bb
Bruno Sales <me@baliestri.dev>
2022-12-06 21:44:40 -0300

    feat(core/entities): add `UserEntity`

---
+++ source/Core/Entities/UserEntity.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using MyFinances.Core.Primitives;
+
+namespace MyFinances.Core.Entities;
+
+public sealed class UserEntity : BaseEntity {
+  public UserEntity() { }
+
+  public UserEntity(string userName, string firstName, string lastName, string email, string password) {
+    UserName = userName;
+    FirstName = firstName;
+    LastName = lastName;
+    Email = email;
+    Password = password;
+  }
+
+  public string UserName { get; set; } = null!;
+  public string FirstName { get; set; } = null!;
+  public string LastName { get; set; } = null!;
+  public string Email { get; set; } = null!;
+  public string Password { get; set; } = null!;
+
+  public string? RefreshToken { get; set; }
+  public DateTime? RefreshTokenExpiration { get; set; }
+}
