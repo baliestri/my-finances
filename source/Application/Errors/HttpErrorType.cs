973281b4c73b84b71d4a38ec8c6b001aecdadd82
Bruno Sales <me@baliestri.dev>
2022-12-06 22:12:02 -0300

    feat(application/errors): add `HttpErrorType` enum

---
+++ source/Application/Errors/HttpErrorType.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+namespace MyFinances.Application.Errors;
+
+public enum HttpErrorType {
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  Conflict = 409,
+  Unexpected = 500
+}
