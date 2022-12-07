ce952799d1f5f1e8b77a74794f6c1b4fe1523f86
Bruno Sales <me@baliestri.dev>
2022-12-06 23:36:55 -0300

    feat(application/cqrs): add extensions for `IServiceCollection`
    add `AddApplicationLayer` method

---
+++ source/Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyFinances.Application;
+
+public static class ServiceCollectionExtensions {
+  public static IServiceCollection AddApplicationLayer(this IServiceCollection serviceCollection)
+    => serviceCollection.AddMediatR(typeof(ServiceCollectionExtensions).Assembly);
+}
