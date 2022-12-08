1516de5b324105382db70c80bddb5b6ba26cc059
Bruno Sales <me@baliestri.dev>
2022-12-07 23:50:51 -0300

    feat(core/entity-repository): add `TransactionType` enum

---
+++ source/Core/Enums/TransactionType.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+namespace MyFinances.Core.Enums;
+
+public enum TransactionType {
+  Income,
+  Expense
+}
