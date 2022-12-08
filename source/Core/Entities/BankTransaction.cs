51e2d5622573f2c5670e943a94c61ae2e9fefa55
Bruno Sales <me@baliestri.dev>
2022-12-07 23:51:23 -0300

    feat(core/entity-repository): update `BankTransaction` entity
    add `Type` property

--- source/Core/Entities/BankTransaction.cs
+++ source/Core/Entities/BankTransaction.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
 // See the LICENSE file in the repository root for full license text.
 
+using MyFinances.Core.Enums;
 using MyFinances.Core.Primitives;
 
 namespace MyFinances.Core.Entities;
@@ -9,12 +10,13 @@ public sealed class BankTransaction : BaseEntity {
   public BankTransaction() { }
 
   public BankTransaction(
-    string title, string? description, decimal amount, DateTime date, BankAccount bankAccount
+    string title, string? description, decimal amount, DateTime date, TransactionType type, BankAccount bankAccount
   ) {
     Title = title;
     Description = description;
     Amount = amount;
     Date = date;
+    Type = type;
     BankAccount = bankAccount;
   }
 
@@ -22,6 +24,7 @@ public sealed class BankTransaction : BaseEntity {
   public string? Description { get; set; }
   public decimal Amount { get; set; }
   public DateTime Date { get; set; }
+  public TransactionType Type { get; set; }
 
   public BankAccount BankAccount { get; set; } = null!;
 }
