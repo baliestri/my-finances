6a74d22b3c4b94a2edbb68016e3c25b7d587ab12
Bruno Sales <me@baliestri.dev>
2022-12-07 23:08:16 -0300

    feat(core/entity-repository): add `Bank` entity

---
+++ source/Core/Entities/Bank.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using MyFinances.Core.Primitives;
+
+namespace MyFinances.Core.Entities;
+
+public sealed class Bank : BaseEntity {
+  public string Name { get; set; } = null!;
+
+  public IEnumerable<BankAccount> Accounts { get; set; } = null!;
+}
