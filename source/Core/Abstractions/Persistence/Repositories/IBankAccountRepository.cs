962527ca8be056314e1fa28e7bdd923e41aef952
Bruno Sales <me@baliestri.dev>
2022-12-07 23:10:55 -0300

    feat(core/entity-repository): add `IBankAccountRepository`

---
+++ source/Core/Abstractions/Persistence/Repositories/IBankAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Bruno Sales <me@baliestri.dev>.Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+using MyFinances.Core.Abstractions.Persistence.Primitives;
+using MyFinances.Core.Entities;
+
+namespace MyFinances.Core.Abstractions.Persistence.Repositories;
+
+public interface IBankAccountRepository : IRepository<BankAccount> {
+  Task<BankAccount?> GetByUserIdAsync(Guid userId);
+}
+
